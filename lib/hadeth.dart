4246b497f5cff0dca66931ea37411b06c4615e73
ahmed-elhoshy <93227407+ahmed-elhoshy@users.noreply.github.com>
2023-09-14 02:55:22 +0300

    added empty hadeth screen

--- lib/hadeth.dart
+++ lib/hadeth.dart
@@ -6,9 +6,7 @@ class HadethScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('mmmmm'),
-      ),
+      appBar: AppBar(),
     );
   }
 }
