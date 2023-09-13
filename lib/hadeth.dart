077be98be6f999dbe61fcedf0085a0fd2cdd29fb
ahmed-elhoshy <93227407+ahmed-elhoshy@users.noreply.github.com>
2023-09-14 02:08:10 +0300

    added empty hadeth screen

---
+++ lib/hadeth.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HadethScreen extends StatelessWidget {
+  static const String routename = 'HadethScreen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('mmmmm'),
+      ),
+    );
+  }
+}
