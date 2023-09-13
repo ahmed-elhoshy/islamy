d6eff4e6e7286db760a692c2f064f4d085ac86f6
ahmed-elhoshy <93227407+ahmed-elhoshy@users.noreply.github.com>
2023-09-14 01:25:59 +0300

    added empty quran screen

---
+++ lib/quran.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class QuranScreen extends StatelessWidget {
+  static const String routename = 'QuranScreen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('fgds'),
+      ),
+    );
+  }
+}
